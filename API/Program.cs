924a597fafc10b7691510415912cda58be42ffb3
Marin Calin <marincalin2551@hotmail.com>
2023-11-14 19:55:17 +0200

    Add CORS and controller services to API configuration
    The API configuration 'Program.cs' was enhanced to include CORS and controller services. This is to ensure the API communicates effectively with the front-end application via localhost:4200 or 127.0.0.1:4200 and handles request appropriately using the controller.

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +15,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(policyBuilder =>
+    {
+        policyBuilder.WithOrigins("http://localhost:4200", "https://localhost:4200", "http://127.0.0.1:4200", "https://127.0.0.1:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
