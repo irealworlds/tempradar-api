542423d9e73d5790b1ebb67fa461fc9da61a91c6
Marin Calin <marincalin2551@hotmail.com>
2023-11-21 02:36:41 +0200

    Refactor Weather API services and enhance nullability checks
    The structures of ForecastWeatherApiService and CurrentWeatherApiService have been refactored for better code structure and reusability. For shared functionality of URI building, an AbstractWeatherApiService was implemented. These services were also moved to the Infrastructure layer for better organization.
    
    Nullability was added to the GetWeatherDetailsAsync method in both PinnedCityWeatherService and IPinnedCityWeatherService to handle cases where a city by id is not found.
    
    The WeatherApiSettings configuration was updated to include BaseUri and the ApiKey was made into an init property, increasing security. These changes were also reflected in the WeatherAPI params in appsettings.json. These commits work to increase project organization, code reuse, and robustness while increasing configuration security and integrity.

---
+++ API.Infrastructure/WeatherApi/Services/CurrentWeatherApiService.cs
@@ -0,0 +1,39 @@
+﻿using API.Domain.Contracts.Configuration;
+using API.Domain.Contracts.Services;
+using API.Domain.Dto;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace API.Infrastructure.WeatherApi.Services;
+
+public class CurrentWeatherApiService(HttpClient client, IOptions<WeatherApiSettings> options) :
+    AbstractWeatherApiService(options),
+    ICurrentWeatherApiService
+{
+    public async Task<CurrentWeatherDto> GetCurrentWeatherAsync(double lat, double lon)
+    {
+        var requestUri = BuildEndpointUri("/current.json", new Dictionary<string, string>
+        {
+            { "q", $"{lat},{lon}" },
+            { "aqi", "yes" }
+        });
+        var response = await client.GetAsync(requestUri);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(
+                $"Failed to retrieve weather forecast data. Status code: {response.StatusCode}"
+            );
+        }
+
+        var json = await response.Content.ReadAsStringAsync();
+        var currentWeatherDto = JsonConvert.DeserializeObject<CurrentWeatherDto>(json);
+
+        if (currentWeatherDto == null)
+        {
+            throw new Exception("Could not parse data returned from WeatherApi.");
+        }
+
+        return currentWeatherDto;
+    }
+}
---
+++ API.Infrastructure/WeatherApi/Services/ForecastWeatherApiService.cs
@@ -0,0 +1,40 @@
+﻿using API.Domain.Contracts.Configuration;
+using API.Domain.Contracts.Services;
+using API.Domain.Dto;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace API.Infrastructure.WeatherApi.Services;
+
+public class ForecastWeatherApiService(HttpClient client, IOptions<WeatherApiSettings> options) :
+    AbstractWeatherApiService(options),
+    IForecastWeatherApiService
+{
+    public async Task<WeatherForecastDto> GetWeatherForecastAsync(double latitude, double longitude)
+    {
+        var requestUri = BuildEndpointUri("/forecast.json", new Dictionary<string, string>
+        {
+            { "q", $"{latitude},{longitude}" },
+            { "days", "3" },
+            { "alerts", "no" }
+        });
+        var response = await client.GetAsync(requestUri);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(
+                $"Failed to retrieve weather forecast data. Status code: {response.StatusCode}"
+            );
+        }
+
+        var json = await response.Content.ReadAsStringAsync();
+        var weatherForecastDto = JsonConvert.DeserializeObject<WeatherForecastDto>(json);
+
+        if (weatherForecastDto == null)
+        {
+            throw new Exception("Could not parse data returned from WeatherApi.");
+        }
+
+        return weatherForecastDto;
+    }
+}
